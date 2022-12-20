8a0a39713d6476e2528ecb31a95fefc4596d003a
unknown <quanghuy6698@gmail.com>
2022-12-21 00:01:49 +0700

    [21/12/2022] update

--- logic_task/main.js
+++ logic_task/main.js
@@ -1,43 +1,139 @@
-// Init GitHub
-const github = new GitHub
+const baseUrl = "https://dog.ceo/api/breed/";
 
-// Init UI
-const ui = new UI
+const dogInput = document.getElementById("dogInput");
+const dogImg = document.getElementById("dogImg");
+const msg = document.getElementById("message");
 
-// Search input
-const searchUser = document.getElementById('searchUser');
-
-// Search input event listener
-searchUser.addEventListener('keyup', delay((e)=>{
-    // Get input text 
-    const userText = e.target.value;
-
-    if(userText !== ''){
-        // make http call 
-        github.getUser(userText)
-        .then(data => {
-            if (data.profile.message === 'Not Found'){
-                // Show alert
-                ui.showAlert('User not found', 'alert alert-danger')
-            }else {
-                // Show profile
-                ui.showProfile(data.profile);
-                ui.showRepos(data.repos);
-            }
-        })
-    }else {
-        // Clear profile
-        ui.clearProfile()
-    }
-},3000))
-
-function delay(callback, ms) {
-    var timer = 0;
-    return function() {
-        var context = this, args = arguments;
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            callback.apply(context, args);
-        }, ms || 0);
-    };
+/**
+ * Delay search
+ */
+function delaySearch() {
+  let searchTxt = dogInput.value;
+  if (searchTxt !== "") {
+    let searchUrl = baseUrl + searchTxt + "/images/random/1";
+    fetch(searchUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status == "success") {
+          dogImg.src = data.message[0];
+          msg.innerHTML = "";
+        } else {
+          dogImg.src = "";
+          msg.innerHTML = "Not found.";
+        }
+      });
+  }
 }
+
+// Add event for input
+let typingTimer;
+let typeInterval = 3000;
+
+dogInput.addEventListener("keyup", () => {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(delaySearch, typeInterval);
+});
+
+// Dog breeds footer
+const dogBreedLst = [
+  "affenpinscher",
+  "african",
+  "airedale",
+  "akita",
+  "appenzeller",
+  "australian",
+  "basenji",
+  "beagle",
+  "bluetick",
+  "borzoi",
+  "bouvier",
+  "boxer",
+  "brabancon",
+  "briard",
+  "buhund",
+  "bulldog",
+  "bullterrier",
+  "cattledog",
+  "chihuahua",
+  "chow",
+  "clumber",
+  "cockapoo",
+  "collie",
+  "coonhound",
+  "corgi",
+  "cotondetulear",
+  "dachshund",
+  "dalmatian",
+  "dane",
+  "deerhound",
+  "dhole",
+  "dingo",
+  "doberman",
+  "elkhound",
+  "entlebucher",
+  "eskimo",
+  "finnish",
+  "frise",
+  "germanshepherd",
+  "greyhound",
+  "groenendael",
+  "havanese",
+  "hound",
+  "husky",
+  "keeshond",
+  "kelpie",
+  "komondor",
+  "kuvasz",
+  "labradoodle",
+  "labrador",
+  "leonberg",
+  "lhasa",
+  "malamute",
+  "malinois",
+  "maltese",
+  "mastiff",
+  "mexicanhairless",
+  "mix",
+  "mountain",
+  "newfoundland",
+  "otterhound",
+  "ovcharka",
+  "papillon",
+  "pekinese",
+  "pembroke",
+  "pinscher",
+  "pitbull",
+  "pointer",
+  "pomeranian",
+  "poodle",
+  "pug",
+  "puggle",
+  "pyrenees",
+  "redbone",
+  "retriever",
+  "ridgeback",
+  "rottweiler",
+  "saluki",
+  "samoyed",
+  "schipperke",
+  "schnauzer",
+  "segugio",
+  "setter",
+  "sharpei",
+  "sheepdog",
+  "shiba",
+  "shihtzu",
+  "spaniel",
+  "springer",
+  "stbernard",
+  "terrier",
+  "tervuren",
+  "vizsla",
+  "waterdog",
+  "weimaraner",
+  "whippet",
+  "wolfhound",
+];
+const dogBreeds = document.getElementById("dogBreeds");
+let dogBreedsStr = dogBreedLst.join(" - ");
+dogBreeds.innerHTML = dogBreedsStr;
